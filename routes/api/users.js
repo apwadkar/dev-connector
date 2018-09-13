fd113b9e3d8cff92edffc711760f902ea2e28e9b
apwadkar <uspecimen@gmail.com>
2018-09-13 01:21:13 -0700

    Added basic validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
 
+const validateRegisterInput = require("../../validation/register");
 const keys = require("../../config/keys");
 const User = require("../../models/User");
 
@@ -18,10 +19,17 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({ email: req.body.email })
         .then((user) => {
             if (user) {
-                return res.status(400).json({ email: "Email already exists" });
+                errors.email = "Email already exists";
+                return res.status(400).json(errors);
             }
             const avatar = gravatar.url(req.body.email, {
                 s: "200",
